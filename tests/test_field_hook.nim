142d23f4e43abcc163a077e11c2330ce74d2fa18
metagn <metagngn@gmail.com>
2026-03-21 18:16:02 +0300

    add test for manual field mapping hook

---
+++ tests/test_field_hook.nim
@@ -0,0 +1,39 @@
+import holo_json, holo_map/fields
+
+type
+  Foo {.inheritable.} = object
+    a {.mapping: "x".}: string
+    case b {.mapping(Json, "y").}: uint8 #range[0..5] # https://github.com/nim-lang/Nim/pull/25585
+    of 0..2:
+      c {.mapping: "tooGeneral", mapping(Json, "z"), mapping: "tooGeneralAgain".}: int
+      #when true: # nim limitation
+      d {.mapping: "t".}: bool
+    else:
+      discard
+    notRenamed: string
+
+  Bar = ref object of Foo
+    e {.mapping: "u".}: int
+
+proc fieldMappingPairs*(foo: typedesc[Foo], group: static MappingGroup): FieldMappingPairs =
+  @{
+    "a": toFieldMapping "x",
+    "b": toFieldMapping "y",
+    "c": toFieldMapping "z",
+    "d": toFieldMapping "t",
+    "e": toFieldMapping "u",
+    "notRenamed": FieldMapping()
+  }
+
+import std/json
+
+let obj1 = Bar(a: "foo", b: 1, c: 123, d: true, notRenamed: "bar", e: 456)
+let ser = toJson(obj1)
+doAssert ser.fromJson(JsonNode) == %*{"u":456,"x":"foo","y":1,"z":123,"t":true,"not_renamed":"bar"}
+let obj2 = fromJson(ser, Bar)
+doAssert obj1.a == obj2.a
+doAssert obj1.b == obj2.b
+doAssert obj1.c == obj2.c
+doAssert obj1.d == obj2.d
+doAssert obj1.e == obj2.e
+doAssert obj1.notRenamed == obj2.notRenamed
