cb09399b62aa7d2af4270fb8cef58ce1d4ba161e
metagn <metagngn@gmail.com>
2026-03-28 19:27:38 +0300

    move bench to own job

---
+++ tasks/bench.nim
@@ -0,0 +1,57 @@
+when (NimMajor, NimMinor) >= (1, 4):
+  when (compiles do: import nimbleutils):
+    import nimbleutils
+    # https://github.com/metagn/nimbleutils
+
+when not declared(runTests):
+  {.error: "bench task not implemented, need nimbleutils".}
+
+# run from project root
+
+type BenchKind = enum
+  Check
+  Run
+  RunJs
+
+proc bench(kind: BenchKind) =
+  let base = "-d:release"
+  var combos = @[
+    base,
+    base & " -d:holoJsonReaderImpl=view",
+    base & " -d:holoJsonReaderImpl=tracked-load",
+    base & " -d:holoJsonReaderImpl=tracked-view"
+  ]
+  case kind
+  of Run:
+    var refcCombos = combos
+    for opt in refcCombos.mitems: opt.add " --mm:refc"
+    var arcCombos = combos
+    for opt in arcCombos.mitems: opt.add " --mm:arc"
+    combos.add refcCombos
+    combos.add arcCombos
+  of RunJs: discard
+  of Check:
+    var withExperimentalViews = combos
+    for opt in withExperimentalViews.mitems: opt.add " --experimental:views"
+    combos.add withExperimentalViews
+  let command =
+    if kind == Check: "check"
+    else: ""
+  let backends =
+    case kind
+    of Check: {c, js}
+    of RunJs: {js}
+    of Run: {c}
+  runTests(
+    @["tests/jsony_original/bench.nim", "tests/jsony_original/bench_parts.nim"],
+    subcommand = command,
+    backends = backends,
+    optionCombos = combos
+  )
+
+if defined(checkBench):
+  bench(Check)
+if defined(runBench):
+  bench(Run)
+if defined(runBenchJs):
+  bench(RunJs)
--- tasks/run_tests.nim
+++ tasks/run_tests.nim
@@ -7,42 +7,7 @@ when not declared(runTests):
   {.error: "tests task not implemented, need nimbleutils".}
 
 # run from project root
-if not defined(runBench):
-  runTests(
-    backends = {c, js, nims},
-    recursiveDir = true
-  )
-
-block bench:
-  let base = "-d:release"
-  var combos = @[
-    base,
-    base & " -d:holoJsonReaderImpl=view",
-    base & " -d:holoJsonReaderImpl=tracked-load",
-    base & " -d:holoJsonReaderImpl=tracked-view"
-  ]
-  if defined(runBench):
-    if not defined(runBenchJs):
-      var refcCombos = combos
-      for opt in refcCombos.mitems: opt.add " --mm:refc"
-      var arcCombos = combos
-      for opt in arcCombos.mitems: opt.add " --mm:arc"
-      combos.add refcCombos
-      combos.add arcCombos
-  else:
-    var withExperimentalViews = combos
-    for opt in withExperimentalViews.mitems: opt.add " --experimental:views"
-    combos.add withExperimentalViews
-  let command =
-    if defined(runBench): ""
-    else: "check"
-  let backends =
-    if defined(runBenchJs): {js}
-    elif defined(runBench): {c}
-    else: {c, js}
-  runTests(
-    @["tests/jsony_original/bench.nim", "tests/jsony_original/bench_parts.nim"],
-    subcommand = command,
-    backends = backends,
-    optionCombos = combos
-  )
+runTests(
+  backends = {c, js, nims},
+  recursiveDir = true
+)
