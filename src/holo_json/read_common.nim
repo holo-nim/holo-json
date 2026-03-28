59f74c6d7b528196275b8372e35371db911ad9f0
metagn <metagngn@gmail.com>
2026-03-28 16:04:15 +0300

    define for reader implementation

--- src/holo_json/read_common.nim
+++ src/holo_json/read_common.nim
@@ -1,9 +1,64 @@
-import ./common, holo_flow/load_reader
-export load_reader
+import ./common
 
-type
-  JsonReader* = LoadReader
-  JsonReaderArg* = var JsonReader
+type JsonReaderImplementation* = enum
+  JsonLoadReader = "load"
+  JsonViewReader = "view"
+  JsonGenericReader = "generic"
 
-proc initJsonReader*(doLineColumn = holoJsonLineColumn): JsonReader {.inline.} =
-  result = initLoadReader(doLineColumn = doLineColumn)
+when defined(nimHasGenericDefine):
+  const holoJsonReaderImpl* {.define.}: JsonReaderImplementation = JsonLoadReader
+  const impl = holoJsonReaderImpl
+else:
+  import std/strutils
+  const holoJsonReaderImpl* {.strdefine.} = $JsonLoadReader
+  const impl = parseEnum[JsonReaderImplementation](holoJsonReaderImpl)
+
+when impl == JsonLoadReader:
+  import holo_flow/load_reader
+  from holo_flow/reader_common import holoReaderDisableLineColumn
+  export load_reader
+
+  type
+    JsonReader* = LoadReader
+    JsonReaderArg* = var JsonReader
+
+  proc initJsonReader*(doLineColumn = holoJsonLineColumn): JsonReader {.inline.} =
+    result = initLoadReader(doLineColumn = doLineColumn)
+  
+  template supportsLineColumn*(reader: JsonReaderArg): bool =
+    not holoReaderDisableLineColumn
+elif impl == JsonViewReader:
+  import holo_flow/[view_reader, reader_common]
+  export view_reader
+
+  type
+    JsonReader* = ViewReader
+    JsonReaderArg* = JsonReader
+
+  template initJsonReader*(doLineColumn = holoJsonLineColumn): JsonReader =
+    ## warning: cannot escape stack
+    var stateImpl = initReadState(doLineColumn)
+    initViewReader(stateImpl)
+  
+  template supportsLineColumn*(reader: JsonReaderArg): bool =
+    not holoReaderDisableLineColumn
+elif impl == JsonGenericReader:
+  template getArgType*[T](_: typedesc[T]): untyped =
+    ## hook to override to turn `T` into an argument type
+    T
+
+  template toArgType(T: untyped): untyped =
+    mixin getArgType
+    getArgType(T)
+  type
+    JsonReader*[T] = T
+    JsonReaderArg*[T] = toArgType(T)
+
+  template initJsonReader*[T](impl: T): JsonReader[T] =
+    impl
+  
+  template supportsLineColumn*[T](reader: T): bool =
+    ## override for reader types that support line/column
+    false
+else:
+  {.error: "unimplemented reader implementation " & $impl.}
